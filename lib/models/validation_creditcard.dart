5b773c0505010567f2eb50dc4f32ed32b3eadd2a
Lucas Mendes <esi.lucasmendes@gmail.com>
2022-10-25 13:43:44 -0300

    Teste de Validação Concluído

--- lib/models/validation_creditcard.dart
+++ lib/models/validation_creditcard.dart
@@ -1,1 +1,63 @@
-class ValidationCreditCard {}
+class ValidationCreditCard {
+  final String numberCard;
+  List<int> list = [];
+  int valueIncrement = 0;
+
+  ValidationCreditCard(this.numberCard) {
+    list = convertInDigits(cleanDigits(numberCard));
+    print(checkCard(list));
+  }
+
+  String cleanDigits(String value) {
+    return value.replaceAll(RegExp(r'[^\d]'), '');
+  }
+
+  List<int> convertInDigits(String value) {
+    return value.split('').map((e) => int.parse(e)).toList();
+  }
+
+  bool checkCard(List<int> list) {
+    List<int> listTemp = [];
+    late int numberSeparator;
+
+    for (int i = 0; i < list.length; i++) {
+      if (i < 15) {
+        listTemp.add(list[i]);
+      } else {
+        numberSeparator = list[i];
+      }
+    }
+
+    for (int i = 0; i < listTemp.length; i++) {
+      if (i % 2 == 0) {
+        valueIncrement += checkNumber(listTemp[i] * 2);
+      } else {
+        valueIncrement += checkNumber(listTemp[i]);
+      }
+    }
+
+    valueIncrement = 10 - (valueIncrement % 10);
+
+    if (valueIncrement == numberSeparator) {
+      return true;
+    } else if (valueIncrement == 10 && numberSeparator == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  int checkNumber(int x) {
+    int value = 0;
+
+    if (x < 9) {
+      return x;
+    } else {
+      List<int> listInt = x.toString().split('').map(int.parse).toList();
+      for (int i = 0; i < listInt.length; i++) {
+        value += listInt[i];
+      }
+      return value;
+    }
+  }
+}
---
+++ test/models/validation_creditcard_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_validation_card/models/validation_creditcard.dart';
+
+void main() {
+  // final l = ValidationCreditCard('4916 6418 5936 9080');
+
+  ValidationCreditCard('4916 6418 5936 9080');
+  ValidationCreditCard('5419 8250 0346 1210');
+}
